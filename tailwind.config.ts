8c85b66af02458a013343e5ca866c52c837b08f1
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-11 18:44:11 +0200

    added dark mode provider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {},
     },
   },
   plugins: [],
+  darkMode: 'class',
 }
 export default config
