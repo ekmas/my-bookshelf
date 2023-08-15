683bf5cc69df20a36cabdca3aae1d29dfe7a22a5
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-15 22:53:47 +0200

    added ClientNavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,13 @@
-'use client'
-
 import React from 'react'
-import logo from '../../public/logo.png'
-import Button from './Button'
+import createClient from '@/lib/supabase-server'
+import ClientNavbar from './ClientNavbar'
 
-export default function Navbar() {
-  return (
-    <nav>
-      <div className="mx-auto flex w-container items-center justify-between px-containerDesktop py-5">
-        <Button variant={'link'} href={'/'}>
-          <img src={logo.src} width={60} alt="logo" />
-        </Button>
-        <div>
-          <Button variant={'link'} href={'/sign-in'} className="mr-10">
-            Sign in
-          </Button>
-          <Button variant={'cta'} href={'/sign-up'}>
-            Sign up
-          </Button>
-        </div>
-      </div>
-    </nav>
-  )
+export default async function Navbar() {
+  const supabase = createClient()
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  return <ClientNavbar session={session} />
 }
