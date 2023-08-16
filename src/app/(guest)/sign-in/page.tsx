f15cdee95d9e8bddf7623add08a58719ad293e76
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-16 22:13:17 +0200

    added sign in page

---
+++ src/app/(guest)/sign-in/page.tsx
@@ -0,0 +1,28 @@
+import SignInForm from './SignInForm'
+import SignInProviders from './SignInProviders'
+
+export default function SignIn() {
+  return (
+    <main className="mx-auto flex h-full min-h-[calc(100dvh-88px-70px)] w-container items-center justify-center px-containerDesktop py-20">
+      <div className="w-[500px]">
+        <h1 className="text-center text-3xl font-bold">Sign in</h1>
+
+        <p className="mt-4 text-center opacity-80">
+          Welcome back! Please enter your details.
+        </p>
+
+        <SignInForm />
+
+        <div className="my-5 flex items-center">
+          <hr className="h-0.5 w-full bg-black opacity-50 dark:bg-white" />
+          <div className="shrink-0 px-3">
+            <p className="text-sm">or sign in with</p>
+          </div>
+          <hr className="h-0.5 w-full bg-black opacity-50 dark:bg-white" />
+        </div>
+
+        <SignInProviders />
+      </div>
+    </main>
+  )
+}
