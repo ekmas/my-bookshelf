b4105dcb0f68b96cc8e78776260cbfda2a69b44b
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-16 22:11:31 +0200

    added redirecting to GuestNavbar

--- src/components/GuestNavbar.tsx
+++ src/components/GuestNavbar.tsx
@@ -1,7 +1,13 @@
+import { Session } from 'inspector'
 import logo from '../../public/logo.png'
 import Button from './Button'
+import { redirect } from 'next/navigation'
 
-export default function GuestNavbar() {
+export default function GuestNavbar({ session }: { session: Session | null }) {
+  if (session) {
+    redirect('/')
+  }
+
   return (
     <nav>
       <div className="mx-auto flex w-container items-center justify-center px-containerDesktop py-[21px]">
