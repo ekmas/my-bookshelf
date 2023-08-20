962b407c9e85881722ef12fbdde670a3e84cfc46
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-20 14:33:06 +0200

    added getRandomNumber

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getRandomNumber() {
+  return Math.floor(Math.random() * 1000)
+}
