689993ecbf8dbb7089ecd89ea8602d87bae9afdc
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-22 21:15:16 +0200

    added profile picture hostnames to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+}
