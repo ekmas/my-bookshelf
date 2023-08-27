11be9fa0d4d78b156d868bf6f3556dcf4f35c71e
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-27 18:07:45 +0200

    updated getRandomNumber and added shuffleArray

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getRandomNumber() {
-  return Math.floor(Math.random() * 1000)
+export function getRandomNumber(num: number) {
+  return Math.floor(Math.random() * num)
+}
+
+export function shuffleArray(array: any[]) {
+  const shuffledArray = [...array]
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+  }
+  return shuffledArray
 }
