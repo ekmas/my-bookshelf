0416118757ade66f4ffd5b0c3c88f1f321d881ee
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-08-27 18:12:15 +0200

    added CarouselWrapper component

---
+++ src/app/(main)/home-components/CarouselWrapper.tsx
@@ -0,0 +1,42 @@
+import Carousel from './Carousel'
+import { getRandomNumber } from '@/lib/utils'
+
+export default async function CarouselWrapper({
+  subject,
+  isRecommended,
+}: {
+  subject: string
+  isRecommended?: boolean
+}) {
+  const res = await fetch(
+    `https://openlibrary.org/subjects/${subject}.json?limit=10&offset=${
+      getRandomNumber(10) * 10
+    }`,
+    {
+      method: 'GET',
+    },
+  )
+
+  if (!res.ok) {
+    console.log(res, subject)
+    throw new Error('There is an issue with open library api')
+  }
+
+  const data = await res.json()
+
+  const subjectName = subject[0].toUpperCase() + subject.slice(1)
+
+  if (data?.works?.length < 5) {
+    return null
+  } else {
+    return (
+      <div className="my-10 rounded-lg bg-secondary dark:bg-darkSecondary">
+        <Carousel
+          subjectName={subjectName}
+          isRecommended={isRecommended}
+          data={data}
+        />
+      </div>
+    )
+  }
+}
