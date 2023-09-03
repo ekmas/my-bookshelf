79a4e26d5e5205173f2bc29f9be2683e817a7338
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-03 19:34:12 +0200

    updated introduction modal

--- src/app/(main)/home-components/IntroductionModal.tsx
+++ src/app/(main)/home-components/IntroductionModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import ReactDom from 'react-dom'
 import IntroductionWrapper from './IntroductionWrapper'
+import Modal from '@/components/Modal'
 
 export default function IntroductionModal({
   isFirstLogin,
@@ -9,36 +9,16 @@ export default function IntroductionModal({
   isFirstLogin: null | boolean
 }) {
   const [isActive, setIsActive] = useState(false)
-  const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
     if (isFirstLogin) {
       setIsActive(true)
-      setTimeout(() => {
-        setIsVisible(true)
-      }, 300)
     }
   }, [isFirstLogin])
 
-  if (!isActive) return null
-
-  return ReactDom.createPortal(
-    <div
-      className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-white/50 transition-all duration-300 dark:bg-black/50"
-      style={{
-        opacity: isVisible ? '1' : '0',
-      }}
-    >
-      <div
-        style={{
-          opacity: isVisible ? '1' : '0',
-          visibility: isVisible ? 'visible' : 'hidden',
-        }}
-        className="relative flex w-[450px] flex-col items-center justify-center rounded-md border-2 border-black/50 bg-bg px-5 py-8 text-center transition-all duration-300 dark:border-white/50 dark:bg-darkBg"
-      >
-        <IntroductionWrapper />
-      </div>
-    </div>,
-    document.getElementById('modal') as HTMLElement,
+  return (
+    <Modal active={isActive} setActive={setIsActive} showCloseButton={false}>
+      <IntroductionWrapper />
+    </Modal>
   )
 }
