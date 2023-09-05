06358bba8d2cd702fdc77f542fd0f7e237600191
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-05 04:49:46 +0200

    style changes and added share button

--- src/app/(main)/bookshelf/BookshelfInfo.tsx
+++ src/app/(main)/bookshelf/BookshelfInfo.tsx
@@ -1,6 +1,12 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import defaultprofilepicture from '@/../public/defaultprofilepicture.png'
 import Link from 'next/link'
+import Button from '@/components/Button'
+import Modal from '@/components/Modal'
+import ShareModal from './ShareModal'
 
 type Props = {
   title: string
@@ -8,21 +14,24 @@ type Props = {
 }
 
 export default function BookshelfInfo({ title, user }: Props) {
+  const [isShareModalActive, setIsShareModalActive] = useState(false)
+
   return (
-    <aside className="fixed top-[88px] h-[calc(100dvh-88px-70px)] max-h-[calc(100dvh-88px-70px)] w-[350px] py-10">
+    <aside className="fixed top-[108px] h-[calc(100dvh-108px-70px)] max-h-[calc(100dvh-108px-70px)] w-[350px] rounded-lg bg-secondary p-5 dark:bg-darkSecondary">
       <h2 className="text-2xl font-bold">{title}</h2>
 
       <Link
         href={`/user/${user.name}`}
-        className="mt-5 flex w-max items-center rounded-lg p-[10px] transition-colors hover:bg-whiteHover dark:hover:bg-blackHover"
+        className="my-6 flex max-w-min items-center rounded-lg p-[10px] transition-colors hover:bg-secondaryHover dark:hover:bg-darkSecondaryHover"
       >
-        <div className="mr-4 rounded-full border border-black/30 dark:border-white/30">
+        <div className="mr-4 min-h-[35px] min-w-[35px] rounded-full border border-black/30 dark:border-white/30">
           {user.profilePicture ? (
             <Image
               width={35}
               height={35}
               src={user.profilePicture}
               alt="profile picture"
+              className="rounded-full"
             />
           ) : (
             <Image
@@ -30,12 +39,30 @@ export default function BookshelfInfo({ title, user }: Props) {
               height={35}
               src={defaultprofilepicture.src}
               alt="profile picture"
+              className="rounded-full"
             />
           )}
         </div>
 
-        <h4 className="font-medium">{user.name}</h4>
+        <h4 className="w-[90%] overflow-hidden text-ellipsis whitespace-nowrap font-medium">
+          {user.name}
+        </h4>
       </Link>
+
+      <Button
+        onClick={() => {
+          setIsShareModalActive(true)
+        }}
+        className="w-full"
+        size={'sm'}
+        variant={'cta'}
+      >
+        Share
+      </Button>
+
+      <Modal active={isShareModalActive} setActive={setIsShareModalActive}>
+        <ShareModal />
+      </Modal>
     </aside>
   )
 }
