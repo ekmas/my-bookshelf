82c5fe6077d7fd0571b1dc69a4f3f4ec463cb4fc
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-06 14:06:49 +0200

    moved new-bookshelf page in authorization-required group

--- src/app/(main)/(authorization-required)/new-bookshelf/page.tsx
+++ src/app/(main)/(authorization-required)/new-bookshelf/page.tsx
@@ -1,18 +1,6 @@
-import createServerComponentClient from '@/lib/supabase-server'
-import { redirect } from 'next/navigation'
 import NewBookshelfMain from './NewBookshelfMain'
 
 export default async function NewBookshelf() {
-  const supabase = createServerComponentClient()
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-
-  if (!session) {
-    redirect('/')
-  }
-
   return (
     <main className="mx-auto h-full min-h-[calc(100dvh-88px-70px)] w-container px-containerDesktop py-20">
       <h2 className="text-center text-3xl font-bold">Create new bookshelf</h2>
