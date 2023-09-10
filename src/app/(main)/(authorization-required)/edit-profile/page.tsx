58e4e85d903e0d41316445674081b5432e3d2c19
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-10 02:17:54 +0200

    added /edit-profile page

---
+++ src/app/(main)/(authorization-required)/edit-profile/page.tsx
@@ -0,0 +1,26 @@
+import EditProfileMain from './EditProfileMain'
+import createServerComponentClient from '@/lib/supabase-server'
+
+export default async function EditProfile() {
+  const supabase = createServerComponentClient()
+
+  const { data, error } = await supabase
+    .from('users')
+    .select()
+    .eq('id', (await supabase.auth.getUser()).data.user?.id)
+
+  if (error) {
+    throw new Error(error.message)
+  }
+
+  return (
+    <main className="mx-auto h-full min-h-[calc(100dvh-88px-70px)] w-container px-containerDesktop py-10">
+      <h2 className="text-center text-3xl font-bold">Edit profile</h2>
+
+      <EditProfileMain
+        currentUsername={data?.at(0).username}
+        profilePictureUrl={data?.at(0).profilePictureUrl}
+      />
+    </main>
+  )
+}
