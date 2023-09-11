5a69a7ec450112598ceb6400c54facca0ddcd141
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-11 10:11:43 +0200

    added global-error, error to /app and removed unnecessary ones

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Button from '@/components/Button'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  return (
+    <div className="mx-auto flex h-full min-h-[calc(100dvh-70px)] w-container flex-col items-center justify-center px-containerDesktop py-20">
+      <h2 className="text-3xl font-bold">An error has occured!</h2>
+      <p className="my-8 text-xl">
+        Error: {error.message || 'Something went wrong'}
+      </p>
+      <div className="flex items-center gap-6">
+        <Button href="/" variant={'cta'} className="text-lg">
+          Go home
+        </Button>
+
+        <Button variant={'cta'} className="text-lg" onClick={() => reset()}>
+          Try again
+        </Button>
+      </div>
+    </div>
+  )
+}
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -11,13 +11,21 @@ export default function GlobalError({
 }) {
   return (
     <html>
-      <body className="flex flex-col items-center justify-center">
-        <h2 className="mb-6 text-center text-2xl font-bold">
-          {error.message || 'Something went wrong.'}
-        </h2>
-        <Button variant={'cta'} onClick={() => reset()}>
-          Try again
-        </Button>
+      <body className="flex h-[100dvh] flex-col items-center justify-center">
+        <div>
+          <h2 className="mb-6 text-center text-2xl font-bold">
+            {error.message || 'Something went wrong.'}
+          </h2>
+          <div className="flex items-center gap-6">
+            <Button href="/" variant={'cta'} className="text-lg">
+              Go home
+            </Button>
+
+            <Button variant={'cta'} className="text-lg" onClick={() => reset()}>
+              Try again
+            </Button>
+          </div>
+        </div>
       </body>
     </html>
   )
