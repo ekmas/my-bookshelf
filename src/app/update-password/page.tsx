e8144b25f86f102db9374ffd24c89d70c3fa22b7
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-13 04:35:25 +0200

    added update password page

---
+++ src/app/update-password/page.tsx
@@ -0,0 +1,21 @@
+import createServerComponentClient from '@/lib/supabase-server'
+import { redirect } from 'next/navigation'
+import UpdatePasswordMain from './UpdatePasswordMain'
+
+export default async function UpdatePassword() {
+  const supabase = createServerComponentClient()
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session) {
+    redirect('/sign-in')
+  }
+
+  return (
+    <main className="mx-auto flex h-[calc(100dvh-70px)] w-container items-center justify-center px-containerDesktop py-10">
+      <UpdatePasswordMain />
+    </main>
+  )
+}
