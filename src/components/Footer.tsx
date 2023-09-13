0435993dc9467a15b8af5018f0c9e432e850c8f0
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-13 13:06:25 +0200

    added links and copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
 import ThemeSwitcher from './ThemeSwitcher'
+import Link from 'next/link'
 
 function Footer() {
   return (
-    <footer className="flex items-center justify-center py-5">
-      <ThemeSwitcher />
+    <footer className="mx-auto w-container px-containerDesktop py-5">
+      <div className="flex items-center justify-between">
+        <div className="h-6 w-6">
+          <ThemeSwitcher />
+        </div>
+
+        <div className="flex items-center gap-10">
+          <Link className="hover:underline" href={'/subjects'}>
+            Subjects
+          </Link>
+          <Link className="hover:underline" href={'/privacy-policy'}>
+            Privacy policy
+          </Link>
+          <Link className="hover:underline" href={'/terms-of-service'}>
+            Terms of service
+          </Link>
+        </div>
+      </div>
+      <div className="mt-10 text-center">
+        &copy; Copyright 2023 - My bookshelf
+      </div>
     </footer>
   )
 }
