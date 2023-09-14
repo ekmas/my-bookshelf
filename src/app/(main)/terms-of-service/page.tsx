c02524975eb71f41d1575864e9721ccc6129bf0a
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-14 23:16:08 +0200

    made tos page responsive

--- src/app/(main)/terms-of-service/page.tsx
+++ src/app/(main)/terms-of-service/page.tsx
@@ -1,11 +1,13 @@
 export default function TermsOfService() {
   return (
-    <div className="mx-auto h-full w-[900px] px-containerDesktop py-10">
-      <h2 className="text-center text-3xl font-bold">Terms of service</h2>
+    <div className="mx-auto h-full w-full max-w-[900px] px-containerDesktop py-10">
+      <h2 className="text-center text-3xl font-bold m500:text-xl">
+        Terms of service
+      </h2>
 
-      <ol className="my-10 list-inside list-decimal leading-relaxed">
+      <ol className="my-10 list-inside list-decimal leading-relaxed m500:text-sm">
         <li>
-          <span className="mb-8 inline-block text-xl font-medium">
+          <span className="mb-8 inline-block text-xl font-medium m500:mb-6 m500:text-lg">
             Acceptance of Terms
           </span>{' '}
           <br /> By using the My-Bookshelf web app ("the App"), you agree to
@@ -13,7 +15,7 @@ export default function TermsOfService() {
           do not agree to these Terms, please do not use the App.
         </li>
         <li>
-          <span className="my-8 inline-block text-xl font-medium">
+          <span className="my-8 inline-block text-xl font-medium m500:mb-6 m500:text-lg">
             User Conduct
           </span>{' '}
           <br />
@@ -21,7 +23,7 @@ export default function TermsOfService() {
           and to comply with all applicable laws and regulations.
         </li>
         <li>
-          <span className="my-8 inline-block text-xl font-medium">
+          <span className="my-8 inline-block text-xl font-medium m500:mb-6 m500:text-lg">
             Termination
           </span>{' '}
           <br />
@@ -30,7 +32,7 @@ export default function TermsOfService() {
           these Terms.
         </li>
         <li>
-          <span className="my-8 inline-block text-xl font-medium">
+          <span className="my-8 inline-block text-xl font-medium m500:mb-6 m500:text-lg">
             Limitation of Liability
           </span>{' '}
           <br />
@@ -38,7 +40,7 @@ export default function TermsOfService() {
           consequential, or punitive damages arising from your use of the App.
         </li>
         <li>
-          <span className="my-8 inline-block text-xl font-medium">
+          <span className="my-8 inline-block text-xl font-medium m500:mb-6 m500:text-lg">
             Changes to Terms
           </span>{' '}
           <br />
