17d1ed26e986f6ce6be95ac0a8879d7715f6a80c
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-14 09:38:05 +0200

    updated tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,10 +29,30 @@ const config: Config = {
       width: {
         container: '1300px',
       },
+      maxWidth: {
+        container: '1300px'
+      },
       padding: {
         containerDesktop: '30px',
         containerMobile: '20px',
       },
+      screens: {
+        'm1280': {'max': '1280px'},
+        'm1050': {'max': '1050px'},
+        'm1150': {'max': '1150px'},
+        'm1000': {'max': '1000px'},
+        'm900': {'max': '900px'},
+        'm700': {'max': '700px'},
+        'm650': {'max': '650px'},
+        'm600': {'max': '600px'},
+        'm550': {'max': '550px'},
+        'm500': {'max': '500px'},
+        'm480': {'max': '480px'},
+        'm450': {'max': '450px'},
+        'm400': {'max': '400px'},
+        'm350': {'max': '350px'},
+        'h550': { 'raw': '(max-height: 550px)' },
+      }
     },
   },
   plugins: [],
