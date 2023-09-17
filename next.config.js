eccfe92409a8c1f67673e0a4375f92addc9b0ea0
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-09-17 16:01:26 +0200

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'covers.openlibrary.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'xijisbzstulzycqmfnjo.supabase.co',
-      },
-    ],
-  },
-}
+const nextConfig = {}
+
+module.exports = nextConfig
